0e4ce232b0bb15c32d005979d25a1483889a0fcd
MAANASAYVSN <89873921+MAANASAYVSN@users.noreply.github.com>
2022-03-09 17:08:47 -0600

    Update settings.py

--- rail/settings.py
+++ rail/settings.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-from pathlib import Path
 import os
-
-MEDIA URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'/media/')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
